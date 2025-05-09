dac069cb1faa9ab5b301c6f71a496b9caf3365c8
Demis Getachew <demisgech@gmail.com>
2025-05-09 17:57:30 +0300

    Build TextFiled Component

--- src/main/java/com/codewithdemis/TestComponent.java
+++ src/main/java/com/codewithdemis/TestComponent.java
@@ -1,33 +1,27 @@
 package com.codewithdemis;
 
-import com.codewithdemis.components.BankButton;
+import com.codewithdemis.components.BankTextField;
 import java.awt.*;
 import javax.swing.*;
 
 public class TestComponent {
         public static void show() {
             JFrame frame = new JFrame("Banking System");
-
-            // Default Primary Button
-            BankButton primaryBtn = new BankButton("Login", "primary");
-
-            // Custom Success Button
-            BankButton successBtn = new BankButton("Success", "success");
-            BankButton secondaryBtn = new BankButton("Secondary", "secondary");
-
-            // Danger Button
-            BankButton dangerBtn = new BankButton("Delete Account", "danger");
-
-
-            frame.setLayout(new FlowLayout());
-
-            frame.add(secondaryBtn);
-            frame.add(primaryBtn);
-            frame.add(successBtn);
-            frame.add(dangerBtn);
-
             frame.setSize(300, 200);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new FlowLayout());
+
+            var textFieldPrimary = new BankTextField("Enter your name?");
+            var textFieldSecondary = new BankTextField("Enter your name?","secondary");
+            var textFieldDanger = new BankTextField("Enter your name?","danger");
+            var textFieldSuccess = new BankTextField("Enter your name?","success");
+            var textFieldWarning = new BankTextField("Enter your name?","warning");
+
+            frame.add(textFieldDanger);
+            frame.add(textFieldPrimary);
+            frame.add(textFieldSecondary);
+            frame.add(textFieldSuccess);
+            frame.add(textFieldWarning);
             frame.setVisible(true);
         }
 
