f75e6109bbf27eacbdd579f7ec66361f79160dd0
Demis Getachew <demisgech@gmail.com>
2025-05-15 16:28:53 +0300

    Refactor: add title to user management page

--- src/main/java/com/codewithdemis/frames/UserManagementPanel.java
+++ src/main/java/com/codewithdemis/frames/UserManagementPanel.java
@@ -2,14 +2,17 @@ package com.codewithdemis.frames;
 
 import com.codewithdemis.components.BankButton;
 import com.codewithdemis.components.BankTextField;
+import com.codewithdemis.db.Database;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.TableCellRenderer;
 
 public class UserManagementPanel extends JPanel {
-    private JTable table;
     private BankTextField searchField;
     private BankButton addButton;
     private BankButton editButton;
@@ -19,12 +22,15 @@ public class UserManagementPanel extends JPanel {
         setLayout(new BorderLayout());
 
         // Search panel
-        JPanel searchPanel = new JPanel();
-        searchField = new BankTextField("Search...");
-        BankButton searchButton = new BankButton("Search","success");
-        searchPanel.add(searchField);
-        searchPanel.add(searchButton);
-        searchPanel.setOpaque(false);
+        JPanel searchPanel = getSearchPanel();
+        JLabel title = new JLabel("Welcome to User Management");
+        title.setFont(new Font("OpenSans",Font.BOLD,28));
+        title.setForeground(new Color(200,200,200));
+
+        var header = new JPanel();
+        header.add(title);
+        header.setOpaque(false);
+//        header.add(searchPanel);
 
         // Users table
         String[] columns = {"ID", "First Name", "Last Name", "Email", "Role"};
@@ -32,9 +38,69 @@ public class UserManagementPanel extends JPanel {
                 {"1", "John", "Doe", "john@example.com", "CUSTOMER"},
                 {"2", "Jane", "Smith", "jane@example.com", "ADMIN"}
         };
-        table = new JTable(data, columns);
 
+        List<Object[]> userData = getUserData();
+        for (var d: data)
+            userData.add(d);
 
+        // Convert List<Object[]> to Object[][]
+        Object[][] dataArray = userData.toArray(new Object[0][]);
+
+        JTable table = new JTable(dataArray, columns);
+
+        styleTable(table);
+
+        JPanel tableWrapper = getTableWrapper();
+        tableWrapper.add(table.getTableHeader(), BorderLayout.NORTH);
+
+        tableWrapper.add(table, BorderLayout.CENTER);
+
+        JScrollPane scrollPane = getScrollPane(tableWrapper);
+
+        //  Action buttons
+        JPanel buttonPanel = getBottomButtonPanel();
+
+//        add(searchPanel, BorderLayout.NORTH);
+        add(header, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private JPanel getSearchPanel() {
+        JPanel searchPanel = new JPanel();
+        searchField = new BankTextField("Search...");
+        BankButton searchButton = new BankButton("Search", "success");
+        searchPanel.add(searchField);
+        searchPanel.add(searchButton);
+        searchPanel.setOpaque(false);
+        return searchPanel;
+    }
+
+    private JPanel getBottomButtonPanel() {
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setOpaque(false);
+
+        addButton = new BankButton("Add User");
+        editButton = new BankButton("Edit");
+        deleteButton = new BankButton("Delete", "danger");
+        buttonPanel.add(addButton);
+        buttonPanel.add(editButton);
+        buttonPanel.add(deleteButton);
+        return buttonPanel;
+    }
+
+    private static JScrollPane getScrollPane(JPanel tableWrapper) {
+        JScrollPane scrollPane = new JScrollPane(tableWrapper,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setOpaque(false);
+        scrollPane.getViewport().setOpaque(false);
+        scrollPane.setBorder(null);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        return scrollPane;
+    }
+
+    private void styleTable(JTable table) {
         table.setFont(new Font("Segoe UI", Font.PLAIN, 16));
         table.setRowHeight(28);
         table.setForeground(new Color(248, 248, 242));
@@ -56,8 +122,40 @@ public class UserManagementPanel extends JPanel {
             }
             table.getColumnModel().getColumn(column).setPreferredWidth(width);
         }
+    }
 
-        // === Scroll with padding ===
+    private static List<Object[]> getUserData() {
+        List<Object[]> userData = new ArrayList<>();
+        try {
+            var instance = Database.getInstance();
+            var connection = instance.getConnection();
+
+            var statement = connection.createStatement();
+            String sql = """
+                    SELECT u.id, u.first_name, u.last_name, u.email, r.name AS role_name
+                    FROM users u
+                    JOIN user_roles ur ON u.id = ur.user_id
+                    JOIN roles r ON ur.role_id = r.id;
+                    """;
+
+            var result = statement.executeQuery(sql);
+
+            while (result.next()) {
+                userData.add(new Object[]{
+                        result.getString("id"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("email"),
+                        result.getString("role_name")
+                });
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return userData;
+    }
+
+    private JPanel getTableWrapper() {
         JPanel tableWrapper = new JPanel(new BorderLayout()) {
             @Override
             protected void paintComponent(Graphics g) {
@@ -70,32 +168,7 @@ public class UserManagementPanel extends JPanel {
         };
         tableWrapper.setOpaque(false);
         tableWrapper.setBorder(new EmptyBorder(20, 30, 30, 30));
-        tableWrapper.add(table.getTableHeader(), BorderLayout.NORTH);
-        tableWrapper.add(table, BorderLayout.CENTER);
-
-        JScrollPane scrollPane = new JScrollPane(tableWrapper,
-                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setOpaque(false);
-        scrollPane.getViewport().setOpaque(false);
-        scrollPane.setBorder(null);
-        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
-
-//        Action buttons
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setOpaque(false);
-
-         addButton = new BankButton("Add User");
-         editButton = new BankButton("Edit");
-         deleteButton = new BankButton("Delete","danger");
-        buttonPanel.add(addButton);
-        buttonPanel.add(editButton);
-        buttonPanel.add(deleteButton);
-
-        add(searchPanel, BorderLayout.NORTH);
-        add(scrollPane, BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
+        return tableWrapper;
     }
 
     @Override
@@ -111,15 +184,15 @@ public class UserManagementPanel extends JPanel {
         g2d.fillRect(0, 0, getWidth(), getHeight());
     }
 
-    public void onAdd(ActionListener listener){
+    public void onAdd(ActionListener listener) {
         addButton.addActionListener(listener);
     }
 
-    public void onEdit(ActionListener listener){
+    public void onEdit(ActionListener listener) {
         editButton.addActionListener(listener);
     }
 
-    public void onDelete(ActionListener listener){
+    public void onDelete(ActionListener listener) {
         deleteButton.addActionListener(listener);
     }
 }
